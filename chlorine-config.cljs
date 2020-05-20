82c17c839258074a316a34d5c1ecae6891e5ae0b
Sean Corfield <sean@corfield.org>
2020-05-20 09:39:52 -0700

    Switch to 0.7.x cljs extensions

--- chlorine-config.cljs
+++ chlorine-config.cljs
@@ -9,30 +9,28 @@
 
 (defn rebl-top-block []
   (let [block (editor/get-top-block)]
-    (when block
+    (when (seq (:text block))
       (-> block
           (update :text wrap-in-rebl-submit)
           (editor/eval-and-render)))))
 
 (defn rebl-block []
   (let [block (editor/get-block)]
-    (when block
+    (when (seq (:text block))
       (-> block
           (update :text wrap-in-rebl-submit)
           (editor/eval-and-render)))))
 
 (defn rebl-selection []
   (let [block (editor/get-selection)]
-    (when block
+    (when (seq (:text block))
       (-> block
-          ;; debugging:
-          (assoc :text (str block))
           (update :text wrap-in-rebl-submit)
           (editor/eval-and-render)))))
 
 (defn rebl-def-var []
   (let [block (editor/get-selection)]
-    (when block
+    (when (seq (:text block))
       (-> block
           (update :text
                   #(str "(def " % ")"))
@@ -41,7 +39,7 @@
 
 (defn rebl-var []
   (let [block (editor/get-var)]
-    (when block
+    (when (seq (:text block))
       (-> block
           (update :text #(str "#'" %))
           (update :text wrap-in-rebl-submit)
@@ -49,7 +47,7 @@
 
 (defn rebl-ns []
   (let [block (editor/get-namespace)]
-    (when block
+    (when (seq (:text block))
       (-> block
           (update :text #(str "(find-ns '" % ")"))
           (update :text wrap-in-rebl-submit)
@@ -57,7 +55,7 @@
 
 (defn rebl-remove-ns []
   (let [block (editor/get-namespace)]
-    (when block
+    (when (seq (:text block))
       (-> block
           (update :text
                   #(str
@@ -68,7 +66,7 @@
 
 (defn rebl-reload-all-ns []
   (let [block (editor/get-namespace)]
-    (when block
+    (when (seq (:text block))
       (-> block
           (update :text
                   #(str
@@ -80,26 +78,37 @@
 
 (defn rebl-doc-var []
   (let [block (editor/get-var)]
-    (when block
+    (when (seq (:text block))
       (-> block
           (update :text
                   #(str
-                    "(java.net.URL. "
-                    "(str \"http://clojuredocs.org/\" "
-                    "(symbol #'" % ")))"))
+                    "(java.net.URL."
+                    " (str \"http://clojuredocs.org/\""
+                    " (-> (str (symbol #'" % "))"
+                    ;; clean up ? ! &
+                    "  (clojure.string/replace \"?\" \"%3f\")"
+                    "  (clojure.string/replace \"!\" \"%21\")"
+                    "  (clojure.string/replace \"&\" \"%26\")"
+                    ")))"))
           (update :text wrap-in-rebl-submit)
           (editor/eval-and-render)))))
 
 (defn rebl-javadoc []
   ;; this should be either selection or var
-  (let [block (editor/get-var)]
-    (when block
-      (-> block
-          (update :text
-                  #(str
-                    "(let [c-o-o " %
-                    " ^Class c (if (instance? Class c-o-o) c-o-o (class c-o-o))] "
-                    "(java.net.URL. "
-                    "((requiring-resolve 'clojure.java.javadoc/javadoc-url) (.getName c))))"))
-          (update :text wrap-in-rebl-submit)
-          (editor/eval-and-render)))))
+  (let [block (editor/get-selection)
+        block (if (< 1 (count (:text block))) block (editor/get-var))]
+      (when (seq (:text block))
+        (-> block
+            (update :text
+                    #(str
+                      "(let [c-o-o " %
+                      " ^Class c (if (instance? Class c-o-o) c-o-o (class c-o-o))] "
+                      " (java.net.URL. "
+                      "  (clojure.string/replace"
+                      "   ((requiring-resolve 'clojure.java.javadoc/javadoc-url)"
+                      "    (.getName c))"
+                      ;; strip inner class
+                      "   #\"\\$[a-zA-Z0-9_]+\" \"\""
+                      ")))"))
+            (update :text wrap-in-rebl-submit)
+            (editor/eval-and-render)))))
